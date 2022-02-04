5d1b64f7b968d01f8663c80a89f093c6570ca6fd
Kelson Teixeira <kelson.teixeira@camara.leg.br>
2022-02-04 16:53:34 -0300

    desafio completo

--- src/index.js
+++ src/index.js
@@ -23,24 +23,27 @@ app.post("/repositories", (request, response) => {
     likes: 0
   };
 
-  return response.json(repository);
+  repositories.push(repository);
+
+  return response.status(201).json(repository);
 });
 
 app.put("/repositories/:id", (request, response) => {
   const { id } = request.params;
-  const updatedRepository = request.body;
+  const { title, url, techs, likes } = request.body;
+  const repositoryUpdate = {};
 
-  repositoryIndex = repositories.findindex(repository => repository.id === id);
+  repositoryIndex = repositories.findIndex(repository => repository.id === id);
 
   if (repositoryIndex < 0) {
     return response.status(404).json({ error: "Repository not found" });
   }
+  
+  if(url)   repositories[repositoryIndex].url = url;
+  if(title) repositories[repositoryIndex].title = title;
+  if(techs) repositories[repositoryIndex].techs = techs;
 
-  const repository = { ...repositories[repositoryIndex], ...updatedRepository };
-
-  repositories[repositoryIndex] = repository;
-
-  return response.json(repository);
+  return response.json(repositories[repositoryIndex]);
 });
 
 app.delete("/repositories/:id", (request, response) => {
@@ -48,7 +51,7 @@ app.delete("/repositories/:id", (request, response) => {
 
   repositoryIndex = repositories.findIndex(repository => repository.id === id);
 
-  if (repositoryIndex > 0) {
+  if (repositoryIndex < 0) {
     return response.status(404).json({ error: "Repository not found" });
   }
 
@@ -66,9 +69,11 @@ app.post("/repositories/:id/like", (request, response) => {
     return response.status(404).json({ error: "Repository not found" });
   }
 
-  const likes = ++repositories[repositoryIndex].likes;
+  const likes = repositories[repositoryIndex].likes++;
 
-  return response.json('likes');
+  const repository = repositories[repositoryIndex];
+
+  return response.json(repository);
 });
 
 module.exports = app;
